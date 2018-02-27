d0d033a25c306aaa5ead2ed2cf21023d0b97129a
Larry Papincak <larry12193@gmail.com>
2018-02-26 23:59:54 -0500

    Added webcam image display as the laser clocks around

--- triangulation.py
+++ triangulation.py
@@ -4,7 +4,6 @@ import sys
 import time
 import random
 import pigpio
-
 import cv2
 
 # Define servo pin number
@@ -36,6 +35,44 @@ if not pi.connected:
 pi.set_servo_pulsewidth(SERVO_PIN, width)
 time.sleep(0.5)
 
+# Define camera object
+cam = cv2.VideoCapture(0)
+
+def updateFrame():
+	ret_val, img = cam.read()
+	cv2.imshow("webcam",img)
+
+def incrementLaser():
+	# Set servo pulse width
+	pi.set_servo_pulsewidth(SERVO_PIN, width)
+	# Increment pulse width
+	width += step
+	# Bound the pulse width within min/max and invert the step when boundary
+	# reached
+	if width < MIN_WIDTH or width > MAX_WIDTH:
+		step = -step
+		width += step
+
+def run():
+    while True:
+        try:
+            # Grab next image and display it
+            updateFrame()
+            # Move laser to next position
+            incrementLaser()
+            # Delay for next frame
+            time.sleep(0.1)
+        except KeyboardInterrupt:
+    		break
+
+    pi.set_servo_pulsewidth(SERVO_PIN, 0)
+    # Stop pigpio
+    pi.stop()
+
+if __name__='__main__':
+    run()
+
+'''
 # Run until keyboard interrupt
 while True:
 
@@ -59,4 +96,4 @@ while True:
 pi.set_servo_pulsewidth(SERVO_PIN, 0)
 # Stop pigpio
 pi.stop()
-
+'''
